11fb3e29e52b5992257737c8729cc98df02e2d36
Bohdan <bogmira777@gmail.com>
2023-01-20 22:52:29 +0100

    Add user task

---
+++ homework/user/User.php
@@ -0,0 +1,36 @@
+<?php
+
+class User
+{
+    private $login;
+    private $password;
+
+    public function __construct($login , $password)
+    {
+        $this->login = $login;
+        $this->password = $password;
+    }
+    public function GetLogin(){
+        return $this->login;
+    }
+    public function SetLogin($login){
+        $this->login = $login;
+    }
+    public function SetPassword($password){
+        return $this->password = $password;
+    }
+    public function IsValid($login, $password){
+        return $this->login == $login && $this->password == $password;
+    }
+    public function __toString()
+    {
+        return $this->login." ".$this->password;
+    }
+}
+
+$user = new User("b.sch20", "password");
+echo $user;
+$user->SetPassword("qwerty");
+if ($user->IsValid("b.sch20", "qwerty")){
+    echo "\nUser profile is correct";
+}
